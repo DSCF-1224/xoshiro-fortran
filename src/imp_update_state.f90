22620dd244d5df4defcfb48cc168040791ccf269
DSCF-1224 <take36fcp@vesta.ocn.ne.jp>
2022-01-03 18:00:20 +0900

    2022.01.03 PM06 1st 1. fix the implementation of the `xoshiro256++`

--- src/imp_update_state.f90
+++ src/imp_update_state.f90
@@ -9,12 +9,13 @@ submodule (pkg_xoshiro) imp_update_state
         ! support variable(s) for this <subroutine>
         integer(INT64) :: temp
 
-        temp               = shiftr( i = generator%state(2) , shift = 17 )
+        temp               = shiftl( i = generator%state(2) , shift = 17                 )
         generator%state(3) =   ieor( i = generator%state(3) , j     = generator%state(1) )
         generator%state(4) =   ieor( i = generator%state(4) , j     = generator%state(2) )
         generator%state(2) =   ieor( i = generator%state(2) , j     = generator%state(3) )
-        generator%state(3) =   ieor( i = generator%state(3) , j     = generator%state(4) )
-        generator%state(4) =   rotl( i = generator%state(4) , shift = 45 )
+        generator%state(1) =   ieor( i = generator%state(1) , j     = generator%state(4) )
+        generator%state(3) =   ieor( i = generator%state(3) , j     = temp               )
+        generator%state(4) =   rotl( i = generator%state(4) , shift = 45                 )
 
         return
 
