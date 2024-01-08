fc03a6f90e1f0e50387febcdf240c23244b05a42
DSCF-1224 <take36fcp@vesta.ocn.ne.jp>
2024-01-09 07:12:53 +0900

    Update the source files - Remove unused `TRANSFER` function

--- src/imp_jump_state.f90
+++ src/imp_jump_state.f90
@@ -3,10 +3,10 @@ submodule (pkg_xoshiro) imp_jump_state
     ! constant(s) for this <submodule>
     integer(INT64) , parameter :: jump_param_xoshiro256plus2( 4 ) = &!
         [&!
-            transfer( source=  1733541517147835066_int64 , mold= 0_INT64 ) , &!
-            transfer( source= -3051731464161248980_int64 , mold= 0_INT64 ) , &!
-            transfer( source= -6244198995065845334_int64 , mold= 0_INT64 ) , &!
-            transfer( source=  4155657270789760540_int64 , mold= 0_INT64 )   &!
+            +1733541517147835066_int64 , &!
+            -3051731464161248980_int64 , &!
+            -6244198995065845334_int64 , &!
+            +4155657270789760540_int64   &!
         ]
 
     integer(INT64) , parameter :: jump_param_xoshiro256star2( size_state_xoshiro256star2 ) = jump_param_xoshiro256plus2(:)
--- src/imp_jump_state_long.f90
+++ src/imp_jump_state_long.f90
@@ -4,10 +4,10 @@ submodule (pkg_xoshiro) imp_jump_state_long
 
     integer(INT64) , parameter :: jump_param_xoshiro256plus2( size_state_xoshiro256plus2 ) = &!
         [&!
-            transfer( source=  8566230491382795199_int64 , mold= 0_INT64 ) , &!
-            transfer( source= -4251311993797857357_int64 , mold= 0_INT64 ) , &!
-            transfer( source=  8606660816089834049_int64 , mold= 0_INT64 ) , &!
-            transfer( source=  4111957640723818037_int64 , mold= 0_INT64 )   &!
+            +8566230491382795199_int64 , &!
+            -4251311993797857357_int64 , &!
+            +8606660816089834049_int64 , &!
+            +4111957640723818037_int64   &!
         ]
 
     integer(INT64) , parameter :: jump_param_xoshiro256star2( size_state_xoshiro256star2 ) = jump_param_xoshiro256plus2(:)
